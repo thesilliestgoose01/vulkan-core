3a6c1c9f722ad56c159daf08aa2ae2c4873b89ed
0x7070 <raykhelson.ielon@gmail.com>
2025-01-20 00:29:53 -0500

    Initial Commit

---
+++ src/main/java/io/sillygoose/vulkancore/VulkanCore.java
@@ -0,0 +1,16 @@
+package io.sillygoose.vulkancore;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VulkanCore implements ModInitializer {
+	public static final String MOD_ID = "vulkan-core";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("/////////////// INITIALIZING VULKAN CORE \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+	}
+}
---
+++ src/main/java/io/sillygoose/vulkancore/VulkanCoreClient.java
@@ -0,0 +1,10 @@
+package io.sillygoose.vulkancore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VulkanCoreClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
