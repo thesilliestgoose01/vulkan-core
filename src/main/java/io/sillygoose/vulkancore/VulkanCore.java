8dc4beea0d23d929c3df94d4092cd0fc573bb43e
0x7070 <raykhelson.ielon@gmail.com>
2025-01-20 00:52:46 -0500

    add item registry helper

--- src/main/java/io/sillygoose/vulkancore/VulkanCore.java
+++ src/main/java/io/sillygoose/vulkancore/VulkanCore.java
@@ -1,5 +1,6 @@
 package io.sillygoose.vulkancore;
 
+import io.sillygoose.vulkancore.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -12,5 +13,6 @@ public class VulkanCore implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("/////////////// INITIALIZING VULKAN CORE \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/io/sillygoose/vulkancore/item/ModItems.java
@@ -0,0 +1,25 @@
+package io.sillygoose.vulkancore.item;
+
+import io.sillygoose.vulkancore.VulkanCore;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item NULL_ITEM = registerItem("null_item", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(VulkanCore.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        VulkanCore.LOGGER.info("Registering Mod items for " + VulkanCore.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(NULL_ITEM);
+        });
+    }
+}
