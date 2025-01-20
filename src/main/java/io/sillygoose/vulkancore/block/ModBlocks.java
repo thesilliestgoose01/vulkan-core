6c549d4feff605d288cc15591097ba0c4b7ec65d
0x7070 <raykhelson.ielon@gmail.com>
2025-01-20 17:24:25 -0500

    remove "INGREDIENTS" tab additions

--- src/main/java/io/sillygoose/vulkancore/block/ModBlocks.java
+++ src/main/java/io/sillygoose/vulkancore/block/ModBlocks.java
@@ -1,11 +1,11 @@
 package io.sillygoose.vulkancore.block;
 
 import io.sillygoose.vulkancore.VulkanCore;
+import io.sillygoose.vulkancore.block.custom.CrateBlock;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.enums.NoteBlockInstrument;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -14,7 +14,22 @@ import net.minecraft.util.Identifier;
 
 public class ModBlocks {
     public static final Block NULL_BLOCK = registerBlock("null_block",
-            new Block(AbstractBlock.Settings.copy(Blocks.TNT).slipperiness(25.0f).sounds(BlockSoundGroup.ANVIL).nonOpaque()));
+            new Block(AbstractBlock.Settings.copy(Blocks.TNT).slipperiness(100.0f).sounds(BlockSoundGroup.ANVIL).nonOpaque()));
+
+    public static final Block POKEBALL_CRATE_BLOCK = registerBlock("pokeball_crate_block",
+            new CrateBlock(AbstractBlock.Settings.create().nonOpaque()));
+
+    public static final Block GREATBALL_CRATE_BLOCK = registerBlock("greatball_crate_block",
+            new CrateBlock(AbstractBlock.Settings.create().nonOpaque()));
+
+    public static final Block ULTRABALL_CRATE_BLOCK = registerBlock("ultraball_crate_block",
+            new CrateBlock(AbstractBlock.Settings.create().nonOpaque()));
+
+    public static final Block LUXURYBALL_CRATE_BLOCK = registerBlock("luxuryball_crate_block",
+            new CrateBlock(AbstractBlock.Settings.create().nonOpaque()));
+
+    public static final Block MASTERBALL_CRATE_BLOCK = registerBlock("masterball_crate_block",
+            new CrateBlock(AbstractBlock.Settings.create().nonOpaque()));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
@@ -28,9 +43,5 @@ public class ModBlocks {
 
     public static void registerModBlocks() {
         VulkanCore.LOGGER.info("/////////////// INITIALIZING VULKAN CORE BLOCKS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
-
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
-            entries.add(ModBlocks.NULL_BLOCK);
-        });
     }
 }
